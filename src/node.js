07c313d53556fa5e8ec351110dee9399b0a9716e
Sapar Atayev <sopa.97@mail.ru>
2019-03-22 00:02:37 +0300

    feat: add methods appendChild, removeChild, remove to Node class

--- src/node.js
+++ src/node.js
@@ -1,22 +1,45 @@
 class Node {
 	constructor(data, priority) {
-
+		this.data=data;
+		this.priority=priority;
+		this.parent=null;
+		this.left=null;
+		this.right=null;
 	}
 
 	appendChild(node) {
-
+		if(!this.left) {
+			this.left=node;
+			node.parent=this;
+		} 
+		else if(!this.right) {
+			this.right=node;
+			node.parent=this;
+		} 
 	}
 
 	removeChild(node) {
-
+		if(this.left==node) {
+			this.left=null;
+			node.parent=null;
+		} 
+		else if(this.right==node) {
+			this.right=null;
+			node.parent=null;
+		}
+		else throw new Error("node is not a child of this");
 	}
 
 	remove() {
-
+		if(this.parent) {
+			this.parent.removeChild(this);
+		}
 	}
 
 	swapWithParent() {
-		
+		if(this.parent) {
+			this.parent.parent=this;
+		}
 	}
 }
 
